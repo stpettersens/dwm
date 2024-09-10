45b288d6d5d2b0b405aeadebe9df6d78619350a2
Sam Saint-Pettersen <s.stpettersen@pm.me>
2024-09-11 00:44:35 +0100

    updated.

--- install_programs_freebsd.sh
+++ install_programs_freebsd.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 # Install third party programs on FreeBSD.
 # Run as root or doas.
-pkg install -y feh librewolf alacritty picom
+pkg install -y feh 
+pkg install -y librewolf 
+pkg install -y alacritty 
+pkg install -y picom
